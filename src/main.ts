b0cbf326adc399fe700e60eb65df159c97c9b845
Azat Nasyrov <azat.nasyrov31@gmail.com>
2023-10-01 01:28:28 +0600

    added setting up sessions and global pipes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import cookieSession from 'cookie-session';
 
 async function bootstrap() {
   const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.use(
+    cookieSession({
+      keys: ['secret'],
+    }),
+  );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
 
   await app.listen(port, () => console.log(`Server started on port: ${port}`));
 }
